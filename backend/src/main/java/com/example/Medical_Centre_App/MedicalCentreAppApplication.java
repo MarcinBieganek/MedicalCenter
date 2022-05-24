6962bbd84f68eb27d56eb696c0407e48b3afbafe
dave <dawid.strojek@gmail.com>
2022-05-24 15:13:12 +0200

    spring boot app init
    initialized spring boot app backend

---
+++ backend/src/main/java/com/example/Medical_Centre_App/MedicalCentreAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Medical_Centre_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCentreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalCentreAppApplication.class, args);
+	}
+
+}
