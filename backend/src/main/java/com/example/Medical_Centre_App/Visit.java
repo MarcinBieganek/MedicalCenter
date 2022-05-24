32abb545fca7f51a26d319a0fe0fc9d013200e30
dave <dawid.strojek@gmail.com>
2022-05-24 15:47:37 +0200

    visit endpoint
    created simple endpoint to check if server works

---
+++ backend/src/main/java/com/example/Medical_Centre_App/Visit.java
@@ -0,0 +1,44 @@
+package com.example.Medical_Centre_App;
+
+public class Visit {
+
+    private final long id;
+    private final long doctorId;
+    private final long patientId;
+    private final int startTime;
+    private final int endTime;
+    private final String day;
+
+    public Visit(long id, long doctorId, long patientId, int startTime, int endTime, String day) {
+        this.id = id;
+        this.doctorId = doctorId;
+        this.patientId = patientId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.day = day;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getDoctorId() {
+        return doctorId;
+    }
+
+    public long getPatientId() {
+        return patientId;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public String getDay() {
+        return day;
+    }
+}
