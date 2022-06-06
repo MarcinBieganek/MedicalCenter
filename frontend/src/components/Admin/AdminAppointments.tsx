d161ed096135bb05dbb6f11f0ff1814decab74ce
MarcinBieganek <marcin.bieganek@gmail.com>
2022-06-06 22:38:46 +0200

    Getting doctors appointments from backend finished

--- frontend/src/components/Admin/AdminAppointments.tsx
+++ frontend/src/components/Admin/AdminAppointments.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
-import { v4 } from 'uuid';
 import api from '../../services/backend';
 
 import IAppointment from '../../types/IAppointment';
@@ -11,16 +10,7 @@ const AdminAppointments = () => {
   const { t } = useTranslation();
   const params = useParams();
 
-  const [appointmentsList, setAppointmentsList] = useState<IAppointment[]>([
-    {
-      id: v4(), patientFirstName: 'Pacjent', patientLastName: 'Nowak', date: '10.05.2022', startHour: '14:15', endHour: '14:30', avilable: true,
-    },
-    {
-      id: v4(), patientFirstName: 'Nowy', patientLastName: 'Pacjent', date: '11.05.2022', startHour: '14:20', endHour: '14:40', avilable: true,
-    },
-    {
-      id: v4(), patientFirstName: 'Pacjent', patientLastName: 'Nowak', date: '12.05.2022', startHour: '10:00', endHour: '10:20', avilable: false,
-    }]);
+  const [appointmentsList, setAppointmentsList] = useState<IAppointment[]>([]);
 
   useEffect(() => {
     const getAppointments = async () => {
@@ -28,15 +18,31 @@ const AdminAppointments = () => {
         const bookedVisitsResponse = await api.get('/bookedvisits');
         const bookedVisits = bookedVisitsResponse.data;
         const doctorBookedVisits = bookedVisits.filter((v) => v.doctorId === params.doctorId);
-        console.log(bookedVisits);
-        console.log(doctorBookedVisits);
+
+        const patientsResponde = await api.get('/patients');
+        const patients = patientsResponde.data;
+
+        const appointments = doctorBookedVisits.map((visit) => {
+          const visitPatient = patients.find((p) => p.id === visit.patientId);
+          return {
+            id: visit.id,
+            patientFirstName: visitPatient.firstName,
+            patientLastName: visitPatient.lastName,
+            startHour: visit.startTime,
+            endHour: visit.endTime,
+            date: visit.day,
+            avilable: visit.isBooked,
+          }
+        });
+
+        setAppointmentsList(appointments);
       } catch (error) {
         console.log(error);
       }
     };
 
     getAppointments();
-  }, []);
+  }, [params.doctorId]);
 
   return (
     <main style={{ padding: '1rem 0' }}>
