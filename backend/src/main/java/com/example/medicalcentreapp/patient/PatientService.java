d4eda48c3f74b75be27247490efafd64bf131c67
MarcinBieganek <marcin.bieganek@gmail.com>
2022-06-07 17:58:48 +0200

    patient panel changes

--- backend/src/main/java/com/example/medicalcentreapp/patient/PatientService.java
+++ backend/src/main/java/com/example/medicalcentreapp/patient/PatientService.java
@@ -1,9 +1,6 @@
 package com.example.medicalcentreapp.patient;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,7 +17,7 @@ public class PatientService {
 
     public Patient getPatientById(String id) {
         for (Patient patient : patients) {
-            if (patient.getId().equals(id)) {
+            if (patient.getId() == id) {
                 return patient;
             }
         }
